cd6e42d38389dfbf83bbe7ecff6d0896f93884fe
Adam <adamayeah@gmail.com.au>
2022-06-16 23:38:37 +1000

    manual Button and File creation

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/atoms/Button/Button.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Button
+          loginregister
+        >
           Work In Progress
-        </p>
+        </Button>
       </header>
     </div>
   );
---
+++ app/frontend/src/components/atoms/Button/Button.js
@@ -0,0 +1,47 @@
+import styled, { css } from 'styled-components'
+
+// const Button = () => {
+//   const GeneralButton = styled.button`
+//       background-color: white;
+//       border-radius: 5px;
+//       display: inline-block;
+//       border-radius: 3px;
+//       padding: 0.5rem 0;
+//       margin: 0.5rem 1rem;
+//       width: 11rem;
+//       color: purple;
+//       border: 2px solid black;
+  
+  
+//       ${props => props.loginregister && css`
+//         background: purple;
+//         color: white;
+//       `}
+//   `; 
+//   return GeneralButton;
+// }
+
+// export default Button;
+
+export const Button = styled.button`
+  background-color: white;
+  border-radius: 5px;
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  color: purple;
+  border: 2px solid black;
+  &:hover {
+    cursor: pointer,
+  }
+
+  ${props => props.loginregister && css`
+    background: purple;
+    color: white;
+  `}
+`; 
+// will need to change the props structure once we're passing through the login/register page
+
+
