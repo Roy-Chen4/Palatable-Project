a6e058a06fa694307664c4bd76cebd3243e134e2
Adam <adamayeah@gmail.com.au>
2022-06-16 23:58:50 +1000

    MUI implementation of button and modal, temporarily placed in App.js until page config is set up

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,18 +1,58 @@
 import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
-import { Button } from './components/atoms/Button/Button.js';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
         <Button
-          loginregister
+          variant="outlined"
+          onClick={handleClickOpen}
         >
-          Work In Progress
+          Hello World!
         </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Register</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We
+              will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Subscribe</Button>
+          </DialogActions>
+        </Dialog>
       </header>
     </div>
   );
