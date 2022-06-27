5f4769d3e084c207460fade405dbe14f655012e9
Adam <adamayeah@gmail.com.au>
2022-06-27 10:58:05 +1000

    centring text input div

--- app/frontend/src/containers/IngredientPage/IngredientPage.js
+++ app/frontend/src/containers/IngredientPage/IngredientPage.js
@@ -2,24 +2,27 @@ import * as React from 'react';
 import { InputAdornment } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import './IngredientPage.css';
 
 function IngredientPage() {
   
     return (
       <div className="IngredientPage">
         <h1>This is the IngredientPage</h1>
-        <TextField
-          variant='outlined'
-          margin='normal'
-          sx={{position: "relative", justifyContent: "center", width: "30%", backgroundColor: "white"}}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment>
-                <SearchIcon/>
-              </InputAdornment>
-            )
-          }}
-        />
+        <div className='TextInput'>
+            <TextField
+            variant='outlined'
+            margin='normal'
+            sx={{position: "relative", display: "flex", justifyContent: "center", alignContent: "center", width: "30%", backgroundColor: "white"}}
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment>
+                        <SearchIcon/>
+                    </InputAdornment>
+                )
+            }}
+            />
+        </div>
       </div>
     );
   }
