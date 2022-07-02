c1b7e5ea949ddd78b4fcafe3a6dfbadc1d6b317d
Adam <adamayeah@gmail.com.au>
2022-07-02 16:26:08 +1000

    createstore method deprecated

--- app/frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import isLoggedReducer from "./isLogged";
-import { combineReducers } from "redux";
-
-const allReducers = combineReducers({
-    isLogged : isLoggedReducer,
-})
-
-export default allReducers;
