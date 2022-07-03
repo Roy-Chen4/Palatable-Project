23fb98bdd9cfa320b866b88fe0856fe23181e975
adam-feng <adamfeng0113@gmail.com>
2022-07-03 20:46:42 +1000

    removing unused code

--- app/frontend/src/components/molecules/Modal/AuthModal.js
+++ app/frontend/src/components/molecules/Modal/AuthModal.js
@@ -1,28 +1,15 @@
 /* eslint-disable no-unused-vars */
-import * as React from 'react';
-import { 
-    Dialog, 
-    DialogContent, 
-    DialogTitle, 
-    DialogActions, 
-    DialogContentText,
-    TextField,
-    createTheme
-} 
-from '@mui/material';
-import Button from '@mui/material/Button';
+import {
+    createTheme, Dialog
+} from '@mui/material';
 import PropTypes from 'prop-types';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { login } from '../../../reducers/isLogged';
+import * as React from 'react';
 import LoginModalBody from '../ModalBody/LoginModalBody';
 import RegisterModalBody from '../ModalBody/RegisterModalBody';
 // TWO FACTOR MODAL REFACTORED
 // import TwoFactorModal from './TwoFactorModal';
 
 export default function AuthModal(props) {
-    const dispatch = useDispatch()
-
     // state of RegisterModal
     const [regOpen, setRegOpen] = React.useState(false);
 
@@ -30,93 +17,20 @@ export default function AuthModal(props) {
     // TWOFACTORMODAL REFACTORED
     // const [twoFactorOpen, setTwoFactorOpen] = React.useState(false);
 
-    // set RegisterModal closed when LoginModal opens
+    // set LoginModal closed when RegisterModal opens
     const toggleLoginModal = () => {
         props.onClose();
         setRegOpen(true);
     };
+    
+    // set RegisterModal closed when LoginModal opens
     const toggleRegisterModal = () => {
         setRegOpen(false);
         props.onToggle();
     };
 
-    // login details formatted for API call
-    const [loginDetails, setLoginDetails] = React.useState({
-        email: "", 
-        password:""
-      });
-
-    // register details formatted for API call
-    const [registerDetails, setRegisterDetails] = React.useState({
-        email: "", 
-        password1:"",
-        password2:""
-    });
-
-    // // onChange function for Login textfields
-    // const handleLoginChange = (event) => {
-    //     // setHasError(false);
-    //     const { name, value } = event.target;
-    //     setLoginDetails( {
-    //         ...loginDetails,
-    //         [name]: value,
-    //     })
-    //   }
-
-    // // onChange function for Register textfields
-    // const handleRegisterChange = (event) => {
-    //     // setHasError(false);
-    //     const { name, value } = event.target;
-    //     setRegisterDetails( {
-    //     ...registerDetails,
-    //     [name]: value,
-    //     })
-    // }
-
-    // handle field clears on submit
-    const clearFields = () => {
-        setLoginDetails( {
-            email: "",
-            password: "",
-          })
-          setRegisterDetails( {
-            email: "",
-            password1: "",
-            password2: "",
-          })
-    }
-
     // const [hasError, setHasError] = React.useState(false);
     // const [errorMessage, setErrorMessage] = React.useState("")
-
-    // register details user calling register API
-    const handleRegisterSubmit = () => {
-        console.log(registerDetails);
-        // TWOFACTORMODAL REFACTORED
-        // setTwoFactorOpen(true)
-        // .then(
-        axios
-            .post("/register/", registerDetails)
-            .then((res) => console.log(res))
-            .then(() => clearFields())
-            .then(() => dispatch(login({ isLogged: true, diet: ''})))
-            .then(() => setRegOpen(false))
-            // .then(() => handleLogin())
-            .catch((err) => {
-                console.log(err.request);
-                console.log(err.request.responseText);
-            //   setHasError(true);
-            //   const obj = JSON.parse(err.request.response);
-            //   if (Object.keys(obj).length === 0) {
-            //     setErrorMessage("Invalid User")
-            //   } else if (obj.email !== undefined) {
-            //     setErrorMessage(obj.email[0])
-            //   } else if (obj.password !== undefined) {
-            //     setErrorMessage(obj.password[0])
-            //   }
-            })
-        // );
-    }
 
     // Primary colour theme for buttons
     const primaryTheme = createTheme({
