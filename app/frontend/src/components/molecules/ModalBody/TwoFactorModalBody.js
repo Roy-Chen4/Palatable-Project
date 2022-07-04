70b700ae96c26acad20a36bb13ae947f8d032992
adam-feng <adamfeng0113@gmail.com>
2022-07-04 23:20:37 +1000

    dev feature and finished off password prompt

--- app/frontend/src/components/molecules/ModalBody/TwoFactorModalBody.js
+++ app/frontend/src/components/molecules/ModalBody/TwoFactorModalBody.js
@@ -9,20 +9,15 @@ import {
     Button
 } 
 from '@mui/material';
-// import validationLogin from "../../../validation/loginSchema";
 import { withFormik } from "formik";
-// import * as yup from "yup";
 import axios from 'axios';
 import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../../reducers/isLogged";
 import { login } from "../../../reducers/isLogged";
-import validationRegister from "../../../validation/regSchema";
 import "./ModalBody.css"
 
 const form = props => {
     const {
         values,
-        // touched,
         errors,
         handleChange,
         handleBlur,
@@ -38,34 +33,32 @@ const form = props => {
 
     const [isSubmitting, setIsSubmitting] = React.useState(false);
 
-    // const [accountError, setAccountError] = React.useState(false);
+    const [codeError, setCodeError] = React.useState(false);
 
     const userEmail = useSelector((state) => state.user.value.email);
 
     // login user calling login API
     const onTwoFactorSubmit = () => {
         setIsSubmitting(true);
-        console.log(values)
-     
-
+        // console.log(values)
         axios
             .post("/twofac/", values)
             .then((res) => console.log(res))
             .then(() => resetForm())
-            .then(() => onClose())
+            .then(() => onSubmit())
             .then(() => registerClose())
             .then(() => setIsSubmitting(false))
-            .then(() => dispatch(login({ isLogged: true, email: values.email, diet: ''})))
+            .then(() => dispatch(login({ isLogged: true, email: userEmail})))
             .catch((err) => {
-                // setAccountError(true);
-                // setIsSubmitting(false);
+                setCodeError(true);
+                setIsSubmitting(false);
                 console.log(err.request);
         });
     }
 
     return (
         <form>
-            <DialogTitle>
+            <DialogTitle onClick={() => onSubmit()}>
                 <p1 className='conf_heading'>
                     Account Confirmation
                 </p1>
@@ -79,16 +72,17 @@ const form = props => {
                     placeholder='AAAAA'
                     type="password"
                     value={values.codeDetail}
-                    onChange={(e) => {handleChange(e)}}
+                    onChange={(e) => {
+                        handleChange(e)
+                        setCodeError(false);
+                    }}
                     onBlur={handleBlur}
-                    //helperText={touched.password ? errors.password : ""}
-                    //error={(touched.password && Boolean(errors.password))}
                     margin="dense"
                     // variant="outlined"
                     sx={{width:"70%"}}
                 />
             </DialogContent>
-            {/* <p1 className="error-text" style={{visibility: accountError ? "visible" : "hidden"}}>Invalid Email or Password</p1> */}
+            <p1 className="error-text" style={{visibility: codeError ? "visible" : "hidden"}}>Invalid Code</p1>
             <DialogActions>
                 <Button 
                     onClick={() => {onClose()}}
@@ -100,9 +94,7 @@ const form = props => {
                 <Button 
                     onClick={() => onTwoFactorSubmit()}
                     variant="contained"
-                    disabled={isSubmitting 
-                        // || errors.email || errors.password
-                    }
+                    disabled={isSubmitting || codeError}
                     theme={primaryTheme}
                     sx={{color:"white"}}
                 > 
@@ -113,8 +105,6 @@ const form = props => {
     );
 };
 
-const TwoFactorModalBody = withFormik({
-    // validationSchema: yup.object().shape(validationLogin),
-})(form);
+const TwoFactorModalBody = withFormik({})(form);
 
 export default TwoFactorModalBody;
