9d7dfcc92b18e8f2b7306e3d807290d1f9fad37e
Roy-Chen4 <roychen882@gmail.com>
2022-07-07 00:09:41 +1000

    Implemented a seach bar that can filter from a json list

--- app/frontend/src/containers/IngredientPage/IngredientPage.js
+++ app/frontend/src/containers/IngredientPage/IngredientPage.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import { InputAdornment } from '@mui/material';
+/* import { InputAdornment } from '@mui/material'; 
 import TextField from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from '@mui/icons-material/Search'; */
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import ButtonBase from '@mui/material/ButtonBase';
 import Typography from '@mui/material/Typography';
 import './IngredientPage.css';
+import SearchBar from './SearchBar';
+/* import React, { useState, useEffect } from "react";
+import Stack from "@mui/material/Stack";
+import { Autocomplete } from "@mui/material/Autocomplete";
+import { Box } from "@mui/system"; */
+
+
 
 const button_disp = [
   {
@@ -109,14 +116,71 @@ const ImageMarked = styled('span')(({ theme }) => ({
   transition: theme.transitions.create('opacity'),
 }));
 
+/* const [jsonResults, setJsonResults] = useState([]);
 
+useEffect(() => {
+  fetch("https://www.balldontlie.io/api/v1/players")
+  .then((response) => response.json())
+  .then((json) => setJsonResults(json.data))
+}, []) */
+
+/* const getFilteredItems = (query, items) => {
+  if (!query) {
+    return items;
+  }
+  return items.filter(ingredient => ingredient.name.includes(query))
+}
+
+const [query, setQuery] = useState("");
+
+const {ingredients} = IngredientList;
+
+/* const {items} = ingredients;
+
+const filteredItems = getFilteredItems(query, items);
+ */
+
+/* const data = [
+  "apple",
+];
+
+const SearchBar = ({setSearchQuery}) => (
+  <form>
+    <TextField
+      id="searchbar"
+      className="text"
+      onChange={(e) => {
+        setSearchQuery(e.target.value);
+      }}
+      label="Enter ingredient"
+      variant="outlined"
+      placeholder="Search..."
+      size="small"
+    />
+    <IconButton type="submit" aria-label="search">
+      <SearchIcon style={{ fill: "blue" }} />
+    </IconButton>
+  </form>
+);
+
+const filterData = (query, data) => {
+  if (!query) {
+    return data;
+  }
+  else {
+    return data.filter((d) => d.toLowerCase().includes(query));
+  }
+};
+
+const [searchQuery, setSearchQuery] = useState("");
+const dataFiltered = filterData(searchQuery, data); */ 
 
 function IngredientPage() {
   
     return (
       <div className="IngredientPage">
         <div className='TextInput'>
-            <TextField
+            {/* <TextField
             variant='outlined'
             margin='normal'
             sx={{position: "relative", display: "flex", justifyContent: "center", alignContent: "center", width: "30%", backgroundColor: "white"}}
@@ -127,8 +191,40 @@ function IngredientPage() {
                     </InputAdornment>
                 )
             }}
-            />
+            /> */}
+            <SearchBar/>
         </div>
+        {/* <div
+      style={{
+        display: "flex",
+        alignSelf: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        padding: 20
+      }}
+    >
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <div style={{ padding: 3 }}>
+        {dataFiltered.map((d) => (
+          <div
+              className="text"
+              style={{
+              padding: 5,
+              justifyContent: "normal",
+              fontSize: 20,
+              color: "blue",
+              margin: 1,
+              width: "250px",
+              BorderColor: "green",
+              borderWidth: "10px"
+            }}
+            key={d.id}
+          >
+            {d}
+          </div>
+        ))}
+      </div>
+    </div> */}
         <div className='IngredientDisplay'>
         <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%'}}>
       {button_disp.map((image) => (
@@ -169,6 +265,6 @@ function IngredientPage() {
       </div>
     );
   }
-  
+
   export default IngredientPage;
   
