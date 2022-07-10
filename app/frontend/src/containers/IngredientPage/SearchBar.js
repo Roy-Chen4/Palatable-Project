f30792124040554009f12c44664c9a9aba65514b
adam-feng <adamfeng0113@gmail.com>
2022-07-10 22:29:18 +1000

    troubleshooting searchbar

--- app/frontend/src/containers/IngredientPage/SearchBar.js
+++ app/frontend/src/containers/IngredientPage/SearchBar.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { Autocomplete, TextField } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
 import { Box } from "@mui/system";
+import { 
+    Button,
+} 
+from '@mui/material';
 
 function SearchBar() {
     const [jsonResults, setJsonResults] = useState([]);
+    const [ingredientName, setIngredientName] = useState('');
 
     useEffect(() => {
     fetch("https://www.balldontlie.io/api/v1/players")
@@ -13,24 +19,39 @@ function SearchBar() {
     }, [])
     console.log(jsonResults);
     return (
-        <Stack sx = {{width:300}}>
-            <Autocomplete 
-                id="Name"
-                getOptionLabel={(jsonResults) => `${jsonResults.first_name} ${jsonResults.last_name}`}
-                options={jsonResults}
-                sx={{width:300}}
-                isOptionEqualToValue={(option,value) => 
-                    option.first_name === value.first_name
-                }
-                noOptionsText={"No availabe selection"}
-                renderOption={(props, jsonResults) => (
-                    <Box component="li" {...props} key={jsonResults.id}>
-                        {jsonResults.first_name} {jsonResults.last_name}
-                    </Box>
-                )}
-                renderInput={(params) => <TextField {...params} label = "Input name"/>}
-            />
-        </Stack>
+        <div>
+            <Stack sx = {{width:300}}>
+                <Autocomplete 
+                    id="Name"
+                    getOptionLabel={(jsonResults) => `${jsonResults.first_name} ${jsonResults.last_name}`}
+                    options={jsonResults}
+                    sx={{width:300}}
+                    isOptionEqualToValue={(option,value) => 
+                        option.first_name === value.first_name
+                    }
+                    noOptionsText={"No availabe selection"}
+                    onChange={(value) => {
+                        console.log(value);
+                        setIngredientName(value.first_name + value.last_name);
+                        console.log(ingredientName)
+                    }}
+                    renderOption={(props, jsonResults) => (
+                        <Box 
+                            component="li" {...props} 
+                            // onClick={() => {
+                            //     setIngredientName(jsonResults.first_name + jsonResults.last_name);
+                                
+                            // }} 
+                            key={jsonResults.id}
+                        >
+                            {jsonResults.first_name} {jsonResults.last_name}
+                        </Box>
+                    )}
+                    renderInput={(params) => <TextField {...params} value={ingredientName} label = "Input name"/>}
+                />
+            </Stack>
+            <Button onClick={()=> console.log(ingredientName)}>Enter</Button>
+        </div>
     );
 }
 
