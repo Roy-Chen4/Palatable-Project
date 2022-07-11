2c13a6c45b2fcb18e36d4b21aa1104d2377a6a4c
adam-feng <adamfeng0113@gmail.com>
2022-07-11 23:17:08 +1000

    finished setting up webpack

---
+++ app/frontend/webpack.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|svg)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+};
